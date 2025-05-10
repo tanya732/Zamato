b86589f7e2b556eeb702bce494a04741432a0740
tarunngusain08 <prudent.tarun0808@gmail.com>
2025-05-10 14:51:12 +0530

    Adding the user and restaurant UI: formatCurrency.ts

---
+++ frontend/apps/user-ui/src/utils/formatCurrency.ts
@@ -0,0 +1,9 @@
+/**
+ * Format a number as currency (USD)
+ */
+export const formatCurrency = (amount: number): string => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount);
+};
